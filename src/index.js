7e4ba4360c6b47dd789bfa63ccfd230fb856358f
Mert Metin <mertmetin@tabletdekor.com>
2021-08-17 18:24:06 +0300

    Get User

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+app.get('/users', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.end('[]');
+});
+var server = app.listen(port, hostname, () => {
+    console.log('Hello World!');
+})
